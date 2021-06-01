4228aa284694bee205741b8128eda5b97b8639b2
Bruno Alexandre Oechsler <oechsler.bruno33@gmail.com>
2021-05-31 21:38:18 -0300

    Collections

---
+++ Intro/Collections.py
@@ -0,0 +1,18 @@
+#Collections
+#Lists = Store anything, store any type
+#Arrays = Numerical data types, must all be the same type
+#Dictionaries = Key/value pairs, storage order not guarateed
+
+bruno = {}
+bruno['first'] = 'Bruno'
+bruno['last'] = 'Oechsler'
+
+jesus = {}
+jesus['first'] = 'Jesus'
+jesus['last'] = 'Cristo'
+
+people = []
+people.append(bruno)
+people.append(jesus)
+
+print(people)
